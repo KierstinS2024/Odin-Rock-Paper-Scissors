ce16280943745df6bf56b52dfe1b994200378ffc
KierstinS2024 <180041125+KierstinS2024@users.noreply.github.com>
2025-06-12 16:02:37 -0600

    erase pseudocode and replace with function names erase initial console.log to make sure js file connects to html file

--- index.js
+++ index.js
@@ -1,11 +1,6 @@
-// PseudoCode for Rock Paper Scissors
-console.log("Rock Paper Scissors Game");
+// Rock, Paper, Scissors Game;
 
 // FUNCTION getHumanChoice
-//     PROMPT user to enter "rock", "paper", or "scissors"
-//     CONVERT input to lowercase
-//     RETURN user choice
-
 function getHumanChoice() {
   let choice = prompt("Enter rock, paper, or scissors:");
   if (choice === null) {
@@ -28,27 +23,13 @@ function getHumanChoice() {
 }
 
 // FUNCTION getComputerChoice
-//     GENERATE a random number between 0 and 2
-//     IF number == 0, RETURN "rock"
-//     ELSE IF number == 1, RETURN "paper"
-//     ELSE RETURN "scissors"
-
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
   const randomIndex = Math.floor(Math.random() * choices.length);
   return choices[randomIndex];
 }
 
-// FUNCTION determineWinner(humanChoice, computerChoice)
-//     IF humanChoice == computerChoice
-//         RETURN "Tie"
-//     ELSE IF (humanChoice == "rock" AND computerChoice == "scissors") OR
-//             (humanChoice == "paper" AND computerChoice == "rock") OR
-//             (humanChoice == "scissors" AND computerChoice == "paper")
-//         RETURN "You win!"
-//     ELSE
-//         RETURN "You lose!"
-
+// FUNCTION determineWinner
 function determineWinner(humanChoice, computerChoice) {
   if (humanChoice === computerChoice) {
     return "Tie";
@@ -62,12 +43,8 @@ function determineWinner(humanChoice, computerChoice) {
     return "You lose!";
   }
 }
-// MAIN FUNCTION playGame
-//     CALL getHumanChoice and store in human
-//     CALL getComputerChoice and store in computer
-//     CALL determineWinner with human and computer
-//     ALERT the result
-//     ASK if user wants to play again
+
+// FUNCTION playGame
 function playGame() {
   let playAgain = true;
 
